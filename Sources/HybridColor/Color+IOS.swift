6759643b0ae178937330e11e4c0fa0e91e4a6e1f
eonist <30n1st@gmail.com>
2024-08-07 20:49:29 +0200

    Clean up hybridcolor lib docs with gpt4

--- Sources/HybridColor/Color+IOS.swift
+++ Sources/HybridColor/Color+IOS.swift
@@ -2,32 +2,56 @@ import SwiftUI
 #if os(iOS)
 import UIKit
 /**
- * - Fixme: ⚠️️ add doc to these, use copilot
+ * - Description: This extension provides UIColor compatibility for Color by adding static variables that represent various shades of system grays, which are adaptable to different appearances in iOS.
  */
 extension Color {
-   public static var darkGray: Color {
-      return .init(uiColor: .darkGray)
-   }
-   public static var lightGray: Color {
-      return .init(uiColor: .lightGray)
-   }
-   public static var systemGray: Color {
-      return .init(uiColor: .systemGray)
-   }
-   public static var systemGray2: Color {
-      return .init(uiColor: .systemGray2)
-   }
-   public static var systemGray3: Color {
-      return .init(uiColor: .systemGray3)
-   }
-   public static var systemGray4: Color {
-      return .init(uiColor: .systemGray4)
-   }
-   public static var systemGray5: Color {
-      return .init(uiColor: .systemGray5)
-   }
-   public static var systemGray6: Color {
-      return .init(uiColor: .systemGray6)
-   }
+    /**
+     A color object with the same color space and component values as the UIKit dark gray color.
+     */
+    public static var darkGray: Color {
+        return .init(uiColor: .darkGray)
+    }
+    /**
+     A color object with the same color space and component values as the UIKit light gray color.
+     */
+    public static var lightGray: Color {
+        return .init(uiColor: .lightGray)
+    }
+    /**
+     A color object that represents the system gray color.
+     */
+    public static var systemGray: Color {
+        return .init(uiColor: .systemGray)
+    }
+    /**
+     A color object that represents the second-level system gray color.
+     */
+    public static var systemGray2: Color {
+        return .init(uiColor: .systemGray2)
+    }
+    /**
+     A color object that represents the third-level system gray color.
+     */
+    public static var systemGray3: Color {
+        return .init(uiColor: .systemGray3)
+    }
+    /**
+     A color object that represents the fourth-level system gray color.
+     */
+    public static var systemGray4: Color {
+        return .init(uiColor: .systemGray4)
+    }
+    /**
+     A color object that represents the fifth-level system gray color.
+     */
+    public static var systemGray5: Color {
+        return .init(uiColor: .systemGray5)
+    }
+    /**
+     A color object that represents the sixth-level system gray color.
+     */
+    public static var systemGray6: Color {
+        return .init(uiColor: .systemGray6)
+    }
 }
 #endif
--- Sources/HybridColor/Color.swift
+++ Sources/HybridColor/Color.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 /**
+ * - Description: This extension provides a way to define dynamic colors that adapt to both light and dark modes in SwiftUI.
  * - Note: More comprehensive: https://stackoverflow.com/a/77707499/5389500
  * - Note: and tut: https://www.jessesquires.com/blog/2023/07/11/creating-dynamic-colors-in-swiftui/
  * - Fixme: ⚠️️ Add support for this for UIColor aswell? there isnt direct support right now, but there exist workarounds, write about these etc
@@ -20,8 +21,8 @@ extension Color {
     */
    public init(light: Color, dark: Color) {
       #if os(macOS)
-      self = Color(nsColor: NSColor(name: "DynamicColor", dynamicProvider: { appearance in
-         if appearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua {
+      self = Color(nsColor: NSColor(name: "DynamicColor", dynamicProvider: { appearance in // fix: doc this line
+         if appearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua { // fix: doc this line
             return NSColor(dark) // Return the color for dark mode
          } else {
             return NSColor(light) // Return the color for light mode
@@ -29,17 +30,18 @@ extension Color {
       }))
       #else
       if #available(iOS 13.0, *) {
-         self = Color(uiColor: UIColor(dynamicProvider: { traitCollection in
-            traitCollection.userInterfaceStyle == .dark ? UIColor(dark) : UIColor(light)
+         self = Color(uiColor: UIColor(dynamicProvider: { traitCollection in // fix: doc this line
+            traitCollection.userInterfaceStyle == .dark ? UIColor(dark) : UIColor(light) // fix: doc this line
          }))
       } else {
-         self = Color(light)
+         self = Color(light) // fix: doc this line
       }
       #endif
    }
 }
 /**
  * Convenience
+ * - Description: This extension provides additional convenience properties for `Color` to easily access common color schemes and system colors, enhancing the adaptability of UI components across different platforms and themes.
  * - Note: Extensive color multiplatform lib: https://github.com/diniska/swiftui-system-colors/blob/void/Sources/SystemColors/SystemColors.swift
  * - Note: theme idea: https://stackoverflow.com/a/72235961/5389500
  * - Note: ref: https://stackoverflow.com/a/62207329/5389500
@@ -49,6 +51,7 @@ extension Color {
 extension Color {
    /**
     * Useful for dark and light mode
+    * - Description: Provides a color that is white in light mode and black in dark mode, useful for background elements that need to adapt to the current UI theme.
     * - Fixme: ⚠️️ Rename to whiteAndBlack? or instead wb? and bw? that would avoid name colision with current implementation code etc
     */
    public static let blackOrWhite: Color = {
@@ -56,6 +59,7 @@ extension Color {
    }()
    /**
     * Useful for dark and light mode
+    * - Description: Provides a color that is black in light mode and white in dark mode, useful for foreground text or UI elements that need to contrast with their background.
     * - Fixme: ⚠️️ Rename to blackOrWhite? or instead bw, that would avoid name colision with current implementation code etc
     */
    public static let whiteOrBlack: Color = {
@@ -63,6 +67,7 @@ extension Color {
    }()
    /**
     * Use background color variants to indicate hierarchy
+    * - Description: Use background color variants to indicate hierarchy in the user interface.
     * - Primary: For the overall view
     * - Secondary: For grouping content within the overall view
     * - Tertiary: For grouping content within secondary elements
